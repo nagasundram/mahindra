be3ce35c68c1cb68de1b55d03bb4c8aa8866816b
ganesh kaliannan <ganesh.kaliannan@gmail.com>
2017-06-21 17:19:01 +0530

    seed data updates

--- app/services/test_data_service.rb
+++ app/services/test_data_service.rb
@@ -1,17 +1,17 @@
 class TestDataService
   def call
-    user = User.find_or_create_by!(email: 'testowner@yopmail.com') do |user|
-        user.password = "12341234"
+    user = User.find_or_create_by!(email: 'mahindraretail@yopmail.com') do |user|
+        user.password = "password"
         user.password_confirmation = "12341234"
         user.store_address = "Test Street"
-        user.store_name = "Apple"
+        user.store_name = "firstcry"
         user.store_code = "1234"
-        user.name = "Naga"
+        user.name = "kinder"
         user.status = "Active"
         user.region = "India"
         user.city = "Bangalore"
         user.state = "KA"
-        user.pincode = "560001"
+        user.pincode = "560025"
       end
       gift_card = GiftCard.find_or_create_by!(card_number: '1234123412341234') do |gd|
         gd.card_number = "1234123412341234",
