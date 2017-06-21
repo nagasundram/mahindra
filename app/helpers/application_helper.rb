c6a81326a34c27bc42576ea764ab2eebbf9e24c7
Nagasundaram <nagasundram@gmail.com>
2017-06-22 03:45:24 +0530

    Tooltip Added for audit remarks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction, search: params[:search]}, {:class => css_class}
   end
+
+  def popover(title, content)
+    content_tag(:i, '', class: "glyphicon glyphicon-info-sign",
+                        id: "#{title}_help",
+                        title: title,
+                        data: {
+                            pop_over: true,
+                            content: content
+                        })
+  end
+
 end
