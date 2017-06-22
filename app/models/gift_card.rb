163376864f64c2a9c4b74b7df37e8833e51622f2
Nagasundaram <nagasundram@gmail.com>
2017-06-22 16:12:14 +0530

    Store Owner walk through fixes.

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -3,6 +3,9 @@ class GiftCard < ApplicationRecord
   has_many :transactions, inverse_of: :gift_card
   validates_numericality_of :balance, greater_than: 0
   validates :card_number, uniqueness: { message: " already exists"}
+  validates :card_number, length: {is: 16, :message => "should be 16 characters"}
+  validates :pin, length: {is: 6, :message => "should be 6 characters"}
+
 
   scope :active_cards, -> { where(status: 1) }
 
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :password_confirmation, :pin]
+Rails.application.config.filter_parameters += [:password, :password_confirmation, :pin, :card_number, :gift_card_id]
