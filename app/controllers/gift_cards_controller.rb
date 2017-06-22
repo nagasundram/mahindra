73cbd57d35cee51aeaa8fb7537431793f9cfdf09
Nagasundaram <nagasundram@gmail.com>
2017-06-22 19:40:06 +0530

    Minor fix

--- app/controllers/gift_cards_controller.rb
+++ app/controllers/gift_cards_controller.rb
@@ -10,9 +10,6 @@ class GiftCardsController < ApplicationController
 
   def validate
     authorize! :validate, GiftCard
-    unless params[:card_number].length == 16 && params[:pin] == 6
-      redirect_to root_path, alert: "Please enter proper card number and pin" and return
-    end
     @gift_card = GiftCard.find_by_card_number_and_pin(params[:card_number], params[:pin])
     if @gift_card.present?
       redirect_to gift_card_path(id: encrypt_data(@gift_card.id))
